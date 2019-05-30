f5210491310b6c26bb2a67821ccc33a56fdcf4c8
Ethan Bensman <ebenz99@gmail.com>
2019-05-30 12:30:34 -0400

    analytics added

---
+++ NBA-headshots/analytics/grapher.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt 
+import numpy as np
+
+
+
+numgraphed = 10000
+
+idx = 0
+darr = np.ndarray(shape=(numgraphed), dtype = "float")
+idxs = np.ndarray(shape=(numgraphed), dtype = "float")
+
+
+with open("accuracy.txt","r") as f:
+	for line in f:
+		first = line.split(",")[0]
+		number = first.split(" ")[-1]
+		darr[idx] = np.float(number)
+		idxs[idx] = np.float(idx)
+		idx += 1
+		if idx >= numgraphed:
+			break
+
+fig = plt.figure()
+ax = plt.axes()
+ax.plot(idxs, darr);
+plt.show()
